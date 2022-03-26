6fdd2562fef2b5f885dd83fcea73e4ff82363c24
bartosz.krochmal <bartosz.krochmal@PK1-10>
2022-03-26 17:30:43 +0100

    Dodaj pliki projektów.

---
+++ Ulamki/Fraction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Ulamki
+{
+    class Fraction:IComparable<Fraction>
+    {
+        int denominator;
+        int meter;
+        public Fraction(int meter, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new Exception("Nie można dzielić przez 0!!!");
+            }
+
+            this.denominator = denominator;
+            this.meter = meter;
+        }
+        public override string ToString()
+        {
+            return $"{meter}/{denominator}";
+        }
+
+        public Fraction add(Fraction secondFraction)
+        {
+            return new Fraction(meter * secondFraction.denominator + secondFraction.meter * denominator, denominator * secondFraction.denominator);
+        }
+
+        public int CompareTo(Fraction fr2)
+        {
+            int num1 = meter * fr2.denominator;
+            int num2 = fr2.meter * denominator;
+
+            if(num1>num2)
+            {
+                return 1;
+
+            }
+            else if(num1<num2)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        public static Fraction operator +(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.meter * fr2.denominator + fr2.meter * fr1.denominator, fr1.denominator * fr2.denominator);
+        }
+        public static Fraction operator -(Fraction fr1)
+        {
+            return new Fraction(-fr1.meter, fr1.denominator);
+        }
+        public static Fraction operator /(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.meter * fr2.denominator, fr1.denominator * fr2.meter);
+        }
+
+    }
+}
---
+++ Ulamki/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ulamki
+{
+    class Compare : IComparer<Fraction>
+    {
+        int IComparer<Fraction>.Compare(Fraction fr1, Fraction fr2)
+        {
+            int num1 = meter * fr2.denominator;
+            int num2 = fr2.meter * denominator;
+
+            if (num1 > num2)
+            {
+                return 1;
+
+            }
+            else if (num1 < num2)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fr1 = new Fraction(1,2);
+            Fraction fr2 = new Fraction(8,2);
+            Fraction fr3 = fr1.add(fr2);
+
+            Console.WriteLine(fr3);
+            Console.WriteLine(fr1 / fr2);
+
+            List<Fraction> list = new List<Fraction>();
+            list.Add(fr1);
+            list.Add(fr2);
+            list.Add(new Fraction(2, 7));
+            list.Add(new Fraction(7, 10));
+            list.Sort();
+
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
